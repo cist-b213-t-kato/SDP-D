5aafff2ce1e1cce37faee58200e154d5d0d4900e
cist-b213-t-kato <cist.b213.t.kato@gmail.com>
2015-12-16 22:41:35 +0900

    stampListPageからリンクできるようにした

--- www/introduce.js
+++ www/introduce.js
@@ -1,13 +1,22 @@
 window.onload = function(){
     var regionId = Number(localStorage.getItem('regionId'));
-    var imagePath = mRegionData[regionId].imagePath;
+    var imagePath;
     var stampName = mRegionData[regionId].name;
     var exp = mRegionData[regionId].explanation;
-    
-    var element = $(
-        '<p>' + stampName + '研究室</p>'
-        + '<p>' + exp + '</p>'
-    );
+    var element;
+    if(localStorage.getItem(regionId)=='acquired'){
+        imagePath = mRegionData[regionId].imagePath;
+        element = $(
+            '<p>' + stampName + '研究室</p>'
+            + '<p>' + exp + '</p>'
+        );
+    }else{
+        imagePath = './ui/images/questionM.png';
+        element = $(
+            '<p>' + stampName + '研究室</p>'
+            + '<p>' + 'まだスタンプが押されていません' + '</p>'
+        );
+    }
     
     $('img#stampimg').attr({"src": imagePath });
     $('p#introduceMessage').append(element);
--- www/stampList.js
+++ www/stampList.js
@@ -8,26 +8,27 @@ for(var rId in mRegionData){
     var tagId = 'region_' + rId;
     
     var imagePath;
+    var element;
     if(localStorage.getItem(rId)=='acquired'){
         imagePath = mRegionData[rId].imagePath;
-        $('#'+tagId).click(function(){
-            var btnid = $(this).attr("id");
-            var getId = btnid.replace('region_', '');
-            localStorage.setItem('regionId', getId);
-            location.href = './index.html';
-            return false;
-        });
     }else{
         imagePath = './ui/images/questionM.png';
     }
-    
-    var element = $(
-        '<span class="stamp" id="' + tagId + '">'
+    element = $(
+        '<div class="stamp" id="' + tagId + '">'
         + '<img src="' + imagePath + '"/><br/>'
         + stampName
-        + '</span>'
+        + '</div>'
     );
     $('#stampList').append(element);
+    $('#'+tagId).click(function(){
+        var btnid = $(this).attr("id");
+        var getId = btnid.replace('region_', '');
+        localStorage.setItem('regionId', getId);
+        location.href = './introducePage.html';
+        return false;
+    });
+    
     
 
     
